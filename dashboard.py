e1b6453d4e6c607bb677000b109a0b9a9a178b8d
Amber <pinkiepief1@gmail.com>
2024-11-21 20:47:50 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ config = json.load(open('config.json'))
 
 dash = Blueprint('dashboard', __name__, template_folder='views', static_folder='views/assets')
 
-API_BASE_URL = 'https://discordapp.com/api'
+API_BASE_URL = 'https://hummus.sys42.net/api'
 AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
 TOKEN_URL = API_BASE_URL + '/oauth2/token'
 
